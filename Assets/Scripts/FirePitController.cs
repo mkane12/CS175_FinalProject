2ffa18ce0dbe83d5950a9dccf55212168ec11181
MichaelaKane <michaelakane@college.harvard.edu>
2017-12-16 12:31:48 -0500

    added non-ignitable firepit

---
+++ Assets/Scripts/FirePitController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePitController : MonoBehaviour {
+
+	// detect if torch flames come into contact with fire pit
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Fire")) {
+	
+
+
+		}
+
+	}
+}
